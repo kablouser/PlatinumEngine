42c108397ddb266a2c608dc903713882ead849dc
ShawnWicky <64520407+ShawnWicky@users.noreply.github.com>
2022-02-07 20:51:42 +0000

    WindowManager
    This is just a test for the windowManger branch. Testing if it is work

---
+++ Include/WindowManager/WindowManager.h
@@ -0,0 +1,14 @@
+//
+// Created by 肖宜辰 on 2022/2/7.
+//
+
+#ifndef PLATINUMENGINE_WINDOWMANAGER_H
+#define PLATINUMENGINE_WINDOWMANAGER_H
+
+
+class WindowManager {
+
+};
+
+
+#endif //PLATINUMENGINE_WINDOWMANAGER_H
---
+++ Source/WindowManager/WindowManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 肖宜辰 on 2022/2/7.
+//
+
+#include "WindowManager/WindowManager.h"
