fa5d77c693a5d97b0b67932cf1abfb44e89a99cd
Erdios <amberrosin@163.com>
2022-02-08 15:57:52 +0000

    Test Glew Lib
    - Code in RasterRenderer.cpp.
    - Add test code to test glew library.
    - Can use VBO now.

--- Source/RasterRenderer/RasterRenderer.cpp
+++ Source/RasterRenderer/RasterRenderer.cpp
@@ -103,8 +103,26 @@ bool RasterRenderer::Update(const sf::Clock& deltaClock)
 
 void RasterRenderer::CreateShader()
 {
-	if (glewInit() != GLEW_OK)
+
+	GLfloat vertices[]=
+			{
+			-0.5f, -0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+			0.0f, 0.0f, 0.0f
+
+			};
+
+	if (glewInit() == GLEW_OK)
 	{
+		glBindBuffer(GL_ARRAY_BUFFER, 10);
+		glBufferData(GL_ARRAY_BUFFER,sizeof(vertices),vertices,GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3 * sizeof(float), (void*)0);
+		glEnableVertexAttribArray(0);
+
+		glUseProgram(0);
+
+
 
 
 	}
