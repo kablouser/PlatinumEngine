7f60340e3a91916898dc5930bece26f4c21717e2
kablouser <kablouser231@gmail.com>
2022-03-01 23:46:37 +0000

    Added comments, and more control to event triggering

--- Include/ComponentComposition/Component.h
+++ Include/ComponentComposition/Component.h
@@ -2,25 +2,66 @@
 
 namespace PlatinumEngine
 {
+	/**
+	 * Compositional thing that can be attached to GameObjects and updated during the game loop.
+	 */
 	class Component
 	{
 	public:
+		/**
+		 * Default constructor. Sets component to enabled.
+		 */
 		Component();
 
-		void SetEnabled(bool isEnabled);
+		explicit Component(bool isEnabled);
 
+		bool GetIsEnabled() const;
+
+		/**
+		 * Sets _isEnabled state and triggers events (OnEnable, OnDisable)
+		 * @param isEnabled is component receiving updates
+		 * @param areEventsTriggered are relevant triggers upon state changes
+		 */
+		void SetEnabled(bool isEnabled, bool areEventsTriggered = true);
+
+		/**
+		 * Gets the GameObject this component is attached to. Could be null.
+		 */
+		// GameObject* GetGameObject() const;
+
+		/**
+		 * Event triggered when this component is added to the scene while the game is running.
+		 * If this component is in the scene before the game starts, it's triggered before the 1st frame of the game.
+		 * If this component is added in the middle of the game, it's triggered as soon as it's added.
+		 */
 		virtual void OnStart();
 
-		virtual void OnDestroy();
+		/**
+		 * Event triggered when this component is *recycled* while the game is running.
+		 * If this component is removed from the scene in the middle of the game, it's triggered as soon as it's removed.
+		 * If this component is in the scene when the game ends, it's triggered after the final frame of the game.
+		 */
+		virtual void OnEnd();
 
+		/**
+		 * Event triggered when _isEnabled state has changed to true while the game is running.
+		 * If this component is already enabled before the start of the game, it's triggered before the 1st frame of the game.
+		 */
 		virtual void OnEnable();
 
+		/**
+		 * Event triggered when _isEnabled state has changed to false while the game is running.
+		 * If this component is already disabled before the start of the game, it's triggered before the 1st frame of the game.
+		 */
 		virtual void OnDisable();
 
-		virtual void OnUpdate();
+		/**
+		 * Event triggered every frame while this component is enabled and the game is running.
+		 */
+		virtual void OnUpdate(float deltaTime);
 
 	private:
 		bool _isEnabled;
-		// GameObject* parent;
+		// GameObject* gameObject;
 	};
 }
--- Source/ComponentComposition/Component.cpp
+++ Source/ComponentComposition/Component.cpp
@@ -5,19 +5,39 @@ namespace PlatinumEngine
 	Component::Component() : _isEnabled(true)
 	{}
 
-	void Component::SetEnabled(bool isEnabled)
+	Component::Component(bool isEnabled) : _isEnabled(isEnabled)
+	{}
+
+	bool Component::GetIsEnabled() const
 	{
+		return _isEnabled;
+	}
+
+	void Component::SetEnabled(bool isEnabled, bool areEventsTriggered)
+	{
+		// don't trigger event's if enabled state is not changed
+		if(_isEnabled == isEnabled)
+			return;
+
 		_isEnabled = isEnabled;
+
+		if(!areEventsTriggered)
+			return;
+
 		if(_isEnabled)
 			OnEnable();
 		else
 			OnDisable();
 	}
 
+	//------------------------------------------------------------------------------------------------------------------
+	// Default empty implementations of events.
+	//------------------------------------------------------------------------------------------------------------------
+
 	void Component::OnStart()
 	{}
 
-	void Component::OnDestroy()
+	void Component::OnEnd()
 	{}
 
 	void Component::OnEnable()
@@ -26,6 +46,6 @@ namespace PlatinumEngine
 	void Component::OnDisable()
 	{}
 
-	void Component::OnUpdate()
+	void Component::OnUpdate(float)
 	{}
 }
