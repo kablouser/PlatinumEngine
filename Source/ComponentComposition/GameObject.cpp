e2e41fde95103761333378e80014d494d8b68109
jasonjk192 <73054054+jasonjk192@users.noreply.github.com>
2022-03-03 14:51:19 +0000

    Update GameObject.cpp

--- Source/ComponentComposition/GameObject.cpp
+++ Source/ComponentComposition/GameObject.cpp
@@ -7,22 +7,27 @@ namespace PlatinumEngine
 	GameObject::GameObject(std::string name): name(name), _hasComponent(false)
 	{}
 
+	//Gets whatever component and assign it. Also set flag to true
 	void GameObject::AddComponent(Component component)
 	{
 		_component = component;
 		_hasComponent = true;
 	}
 
+	//Will just return flag's state. Makes things easier
 	bool GameObject::HasComponent()
 	{
 		return _hasComponent;
 	}
 
+	//Just returns whatever component is there
+	//Will need to decide on Components to support different types
 	Component GameObject::GetComponent()
 	{
 		return _component;
 	}
 
+	//Sets the pointer to NULL although setting flag to false should suffice
 	void GameObject::RemoveComponent()
 	{
 		Component *ptr = &_component;
@@ -30,6 +35,7 @@ namespace PlatinumEngine
 		_hasComponent = false;
 	}
 
+	// Will need to probably decide how different Components are created
 	bool GameObject::HasLightComponent()
 	{
 
