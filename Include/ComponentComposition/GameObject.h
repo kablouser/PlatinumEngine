92a20184df3801fb9a41e8f5a8dec3be2a57073d
jasonjk192 <73054054+jasonjk192@users.noreply.github.com>
2022-03-07 12:58:36 +0000

    Small intermediate update
    -Small improvements to allow multiple components
    -Child/Parent structure for gameobjects
    -Will need to find a way to get Component's type and use it to implement/improve some functionalities

--- Include/ComponentComposition/GameObject.h
+++ Include/ComponentComposition/GameObject.h
@@ -1,6 +1,7 @@
 #ifndef PLATINUMENGINE_GAMEOBJECT_H
 #define PLATINUMENGINE_GAMEOBJECT_H
 #include <string>
+#include <vector>
 #include "ComponentComposition/Component.h"
 namespace PlatinumEngine
 {
@@ -11,17 +12,29 @@ namespace PlatinumEngine
 	public:
 		GameObject();
 		GameObject(std::string name);
-		void AddComponent(Component component);
+
+		bool IsEnabled();
+		void SetEnabled(bool enableFlag);
+
+		GameObject* GetParent();
+		void SetParent(GameObject* parent);
+
+		int GetChildrenCount();
+		GameObject* GetChild(int index);
+
+		void AddComponent(Component* component);
 		bool HasComponent();
-		Component GetComponent();
-		void RemoveComponent();
+		Component* GetComponent(int index);
+		void RemoveComponent(int index);
 
 		bool HasLightComponent();
 		bool HasCameraComponent();
 
 	private:
-		Component _component;
-		bool _hasComponent;
+		bool _isEnabled;
+		GameObject* _parent;
+		std::vector<GameObject*> _children;
+		std::vector<Component*> _components;
 	};
 }
 #endif //PLATINUMENGINE_GAMEOBJECT_H
