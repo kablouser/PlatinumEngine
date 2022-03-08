15ee1712c5e6379bf88a0a7fe2693beccaa4088a
Matthew Cumber <ll16m23c@leeds.ac.uk>
2022-03-08 11:02:34 +0000

    Initial Headers

---
+++ Include/OpenGL/Mesh.h
@@ -0,0 +1,31 @@
+//
+// Created by Matt on 08/03/2022.
+//
+
+#pragma once
+
+#include <glm/glm.hpp>
+#include <vector>
+#include <OpenGL/Vertex.h>
+#include <OpenGL/Texture.h>
+
+namespace PlatinumEngine {
+	/**
+	 * Data structure to store mesh data
+	 * Assumes triangle faces for now
+	 */
+	class Mesh {
+	public:
+		Mesh();
+		Mesh(const std::vector<Vertex> &vertices, const std::vector<unsigned int> &indices);
+		std::vector<Vertex> GetVertices() const;
+		void SetVertices(std::vector<Vertex> &vertices);
+		std::vector<unsigned int> GetIndices() const;
+		void SetIndices(std::vector<unsigned int> &indices);
+
+	private:
+		std::vector<Vertex> _vertices;
+		std::vector<unsigned int> _indices;
+//		Texture *_texture;
+	};
+}
---
+++ Include/OpenGL/Vertex.h
@@ -0,0 +1,16 @@
+//
+// Created by Matt & Jinyuan on 08/03/2022.
+//
+
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace PlatinumEngine {
+	struct Vertex {
+		glm::vec3 position;
+		glm::vec3 normal;
+		glm::vec2 textureCoords;
+	};
+}
+
