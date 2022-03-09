52d5b7009617e620f83db1c47565917e676d924f
jasonjk192 <73054054+jasonjk192@users.noreply.github.com>
2022-03-09 00:55:02 +0000

    Rewritten some functions
    -Made use of templates to handle components of any type (theoretically)
    -Made use of a mapper to ease the understanding of types and stuff (for comparisons and more)
    [Thanks to Matt for the help]
    
    -Code still needs checking, testing as well as proper comments and cleanup in the future

--- Include/ComponentComposition/GameObject.h
+++ Include/ComponentComposition/GameObject.h
@@ -2,9 +2,21 @@
 #define PLATINUMENGINE_GAMEOBJECT_H
 #include <string>
 #include <vector>
+#include <typeinfo>
+#include <typeindex>
 #include "ComponentComposition/Component.h"
+#include "iostream"
+#include <map>
+
+#include "ComponentComposition/ComponentType.h"
+
 namespace PlatinumEngine
 {
+	static std::map<std::type_index, std::string> typeToStringMap = {
+			{std::type_index(typeid(Component)), "Component" },
+			{std::type_index(typeid(MeshComponent)), "MeshComponent"},
+			{std::type_index(typeid(LightComponent)), "LightComponent"}};
+
 	class GameObject
 	{
 	public:
@@ -23,12 +35,57 @@ namespace PlatinumEngine
 		GameObject* GetChild(int index);
 
 		void AddComponent(Component* component);
-		bool HasComponent();
 		Component* GetComponent(int index);
 		void RemoveComponent(int index);
+		int GetComponentCount();
 
-		bool HasLightComponent();
-		bool HasCameraComponent();
+		template <class T> void AddComponent (T *component)
+		{
+			_components.emplace_back(component);
+		}
+
+		template<class T> bool HasComponent()
+		{
+			std::string otherComponentType = typeToStringMap[std::type_index(typeid(T))];
+			for(auto& c:_components)
+			{
+				std::string mappedType = typeToStringMap[std::type_index(typeid(*c))];
+				if(mappedType==otherComponentType)
+					return true;
+			}
+			return false;
+		}
+
+		template<class T> T* GetComponent()
+		{
+			std::string otherType = typeToStringMap[std::type_index(typeid(T))];
+			for(auto& c:_components)
+			{
+				std::string mappedType = typeToStringMap[std::type_index(typeid(*c))];
+				if(mappedType==otherType)
+					return dynamic_cast<T*>(c);
+			}
+			return NULL;
+		}
+
+		template<class T> void RemoveComponent()
+		{
+			std::string otherComponentType = typeToStringMap[std::type_index(typeid(T))];
+			int index=-1, counter=0;
+			for(auto& c:_components)
+			{
+				std::string mappedType = typeToStringMap[std::type_index(typeid(*c))];
+				if(mappedType==otherComponentType)
+				{
+					index=counter;
+					break;
+				}
+				counter++;
+			}
+			if(index>=0)
+				RemoveComponent(index);
+		}
+
 
 	private:
 		bool _isEnabled;
