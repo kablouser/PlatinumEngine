d995081509a2f1c6a10822ee495aac341f142c24
Erdios <amberrosin@163.com>
2022-03-12 18:44:39 +0000

    Change main.cpp back...
    Forgot to remove the test code. Change main.cpp back...

--- Source/main.cpp
+++ Source/main.cpp
@@ -13,22 +13,13 @@
 
 #include <OpenGL/GLCheck.h>
 
-#include <Maths/Matrices.h>
 
 static void GlfwErrorCallback(int error, const char* description)
 {
 	std::cerr << "Glfw Error " << error << ": " << description << std::endl;
 }
 
-int main()
-{
-
-	PlatinumEngine::Maths::Mat4 createMat4(1.0f);
-	std::cout<<createMat4<<std::endl;
-
-}
-
-int main2(int, char**)
+int main(int, char**)
 {
 	// Setup window
 	glfwSetErrorCallback(GlfwErrorCallback);
