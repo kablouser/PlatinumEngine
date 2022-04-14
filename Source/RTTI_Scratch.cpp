2b9a63526463c595c3439117b62338097ef1cd42
kablouser <kablouser231@gmail.com>
2022-04-14 13:55:45 +0100

    Update prototype

--- Source/RTTI_Scratch.cpp
+++ Source/RTTI_Scratch.cpp
@@ -1,7 +1,19 @@
 #include <string_view>
+<<<<<<< HEAD
+#include <vector>
+#include <iostream>
+#include <functional>
+#include <cassert>
+#include <memory>
+
+template<typename T>
+constexpr auto type_name()
+{
+=======
 
 template <typename T>
 constexpr auto type_name() {
+>>>>>>> 2186aba9ac816ed94308baf13fd08cd1c8075ed7
 	std::string_view name, prefix, suffix;
 #ifdef __clang__
 	name = __PRETTY_FUNCTION__;
@@ -21,6 +33,63 @@ constexpr auto type_name() {
 	return name;
 }
 
+<<<<<<< HEAD
+template<class... T>
+std::vector<const char*> getTypeNames()
+{
+	return {typeid(T).name()...};
+}
+
+class DynamicFieldIterator
+{
+public:
+
+	// not needed, but C++ is really inconsistent
+	virtual ~DynamicFieldIterator()	{}
+
+	// interface
+  	virtual bool MoveNext() = 0;
+	virtual void* GetCurrent() = 0;
+	virtual size_t GetSize() = 0;
+};
+
+class VectorFieldIterator : public DynamicFieldIterator
+{
+public:
+
+	VectorFieldIterator(const std::vector<float>& toIterateOver) :
+			_toIterateOver(toIterateOver),
+			_currentIndex(0)
+	{
+	}
+
+	bool MoveNext() override
+	{
+		std::cout << "MoveNext" << std::endl;
+		if (_currentIndex < _toIterateOver.size())
+		{
+			++_currentIndex;
+			return true;
+		}
+		return false;
+	}
+
+	void* GetCurrent() override
+	{
+		return (void*)&_toIterateOver.at(_currentIndex);
+	}
+
+	size_t GetSize() override
+	{
+		return _toIterateOver.size();
+	}
+
+private:
+
+	const std::vector<float>& _toIterateOver;
+	size_t _currentIndex;
+};
+=======
 #include <vector>
 #include <iostream>
 #include <functional>
@@ -36,23 +105,147 @@ int main()
 #include <string>
 #include <vector>
 #include <typeindex>
+>>>>>>> 2186aba9ac816ed94308baf13fd08cd1c8075ed7
 
 class TypeInfo;
 
 class FieldInfo
 {
 public:
+<<<<<<< HEAD
+	std::string typeName;
+	std::string fieldName;
+	size_t offset;
+
+	void* AccessField(void* typeInstance)
+	{
+		return (char*)typeInstance + offset;
+	}
+=======
 	TypeInfo* typeInfo;
 	std::string fieldName;
 
 	void* AccessGeneric(void* instance);
 	int& AccessInt(void* instance);
 	double& AccessDouble(void* instance);
+>>>>>>> 2186aba9ac816ed94308baf13fd08cd1c8075ed7
 };
 
 class TypeInfo
 {
 public:
+<<<<<<< HEAD
+	std::string name; // should be unique?
+
+	bool isTemplate;
+	std::string templateArgument0;
+
+	std::vector<FieldInfo> fields;
+	// can be elements in an array type
+	// but not in the fields!
+	std::function<std::shared_ptr<DynamicFieldIterator>(void* typeInstance)> getDynamicFieldIterator;
+
+	void Serialize();
+
+	void Deserialize();
+
+	void* AccessGeneric(void* typeInstance);
+
+	int& AccessInt(void* typeInstance);
+
+	double& AccessDouble(void* typeInstance);
+};
+
+int main()
+{
+	const volatile static int x = 0;
+	std::cout << "type name = " << type_name<decltype(x)>() << std::endl;
+
+	float myFloat;
+	TypeInfo floatTypeInfo{
+			"float",
+			false,
+			{},
+			{}
+	};
+
+	struct MyStruct
+	{
+		float field0;
+		float field1;
+	};
+	TypeInfo myStructTypeInfo{
+		"MyStruct",
+		false,
+		{},
+		{
+				{"float", "field0", offsetof(MyStruct, field0)},
+				{"float", "field1", offsetof(MyStruct, field1)}
+		}
+	};
+
+	{
+		MyStruct myStructInstance;
+		auto field0TrueOff = (char*)&myStructInstance.field0 - (char*)&myStructInstance;
+		auto field1TrueOff = (char*)&myStructInstance.field1 - (char*)&myStructInstance;
+		assert(field0TrueOff == myStructTypeInfo.fields[0].offset && "field0 offset test");
+		assert(field1TrueOff == myStructTypeInfo.fields[1].offset && "field1 offset test");
+	}
+
+	class BaseClass
+	{
+	public:
+		float baseField0;
+
+		virtual void Method()
+		{
+
+		};
+	};
+
+	class ChildClass : public BaseClass
+	{
+	public:
+		float childField0;
+
+		void Method() override
+		{
+
+		}
+	};
+
+	ChildClass *dangerousPointer = 0;
+	TypeInfo myChildClassTypeInfo{
+			"ChildClass",
+			false,
+			{},
+			{
+					{"float", "baseField0", (size_t)&dangerousPointer->baseField0 },
+					{"float", "childField0", (size_t)&dangerousPointer->childField0 }
+			}
+	};
+
+	{
+		ChildClass myChildClassInstance;
+		auto field0TrueOff = (char*)&myChildClassInstance.baseField0 - (char*)&myChildClassInstance;
+		auto field1TrueOff = (char*)&myChildClassInstance.childField0 - (char*)&myChildClassInstance;
+		assert(field0TrueOff == myChildClassTypeInfo.fields[0].offset && "base field0 offset test");
+		assert(field1TrueOff == myChildClassTypeInfo.fields[1].offset && "child field0 offset test");
+
+		std::cout << "field0 true off " << field0TrueOff << std::endl;
+		std::cout << "field1 true off " << field1TrueOff << std::endl;
+	}
+
+	std::vector<float> myFloats {0,1,2};
+	std::shared_ptr<DynamicFieldIterator> iterator = std::make_shared<VectorFieldIterator>(myFloats);
+
+
+
+	decltype(iterator);
+
+	return 0;
+}
+=======
 	std::string name; // not unique, but "cross-platform"
 	std::type_index index; // unique for each type
 
@@ -61,3 +254,4 @@ public:
 	void Serialize();
 	void Deserialize();
 };
+>>>>>>> 2186aba9ac816ed94308baf13fd08cd1c8075ed7
