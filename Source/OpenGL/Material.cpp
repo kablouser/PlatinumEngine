1dc2eef38096edc5399680a9f38e0850f619fb2d
kablouser <kablouser231@gmail.com>
2022-05-10 23:08:06 +0100

    Jump and foot step SFX

--- Source/OpenGL/Material.cpp
+++ Source/OpenGL/Material.cpp
@@ -7,25 +7,19 @@ namespace PlatinumEngine
 {
 	void Material::CreateTypeInfo(TypeDatabase& typeDatabase)
 	{
-		/*
-		SavedReference<Texture>			diffuseTexture;
-		SavedReference<Texture>			normalTexture;
-		SavedReference<Texture>			specularTexture;
-		float         	shininessFactor = 8.0f;
-		bool useBlinnPhong = false;
-		bool useTexture = false;
-		bool useNormalTexture = false;
-		 */
 		typeDatabase.BeginTypeInfo<Material>()
 		        .WithField<SavedReference<Texture>>("diffuseTexture", PLATINUM_OFFSETOF(Material, diffuseTexture))
 				.WithField<SavedReference<Texture>>("normalTexture", PLATINUM_OFFSETOF(Material, normalTexture))
 				.WithField<SavedReference<Texture>>("specularTexture", PLATINUM_OFFSETOF(Material, specularTexture))
 
 				.WithField<float>("shininessFactor", PLATINUM_OFFSETOF(Material, shininessFactor))
+				.WithField<float>("refractionIndex", PLATINUM_OFFSETOF(Material, refractionIndex))
 
 				.WithField<bool>("useBlinnPhong", PLATINUM_OFFSETOF(Material, useBlinnPhong))
 				.WithField<bool>("useTexture", PLATINUM_OFFSETOF(Material, useTexture))
 				.WithField<bool>("useNormalTexture", PLATINUM_OFFSETOF(Material, useNormalTexture))
+				.WithField<bool>("useReflectionShader", PLATINUM_OFFSETOF(Material, useReflectionShader))
+				.WithField<bool>("useRefractionShader", PLATINUM_OFFSETOF(Material, useRefractionShader))
 				.WithField<Maths::Vec3>("colour", PLATINUM_OFFSETOF(Material, colour));
 	}
 
